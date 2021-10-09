554377d3c89d57d75e5ff1a003f213290b895f7c
Quan Guo <guotsuan@gmail.com>
2021-10-10 01:15:24 +0800

    Make grdata read_complete_file() working

--- iqtools/grdata.py
+++ iqtools/grdata.py
@@ -16,13 +16,24 @@ class GRData(IQBase):
     def __init__(self, filename, fs, center=0, date_time=""):
         super().__init__(filename)
         # Additional fields in this subclass
-        self.date_time = date_time
+        self.fs = fs
         self.center = center
+        self.date_time = date_time
         # each complex64 sample is 8 bytes on disk
         self.nsamples_total = os.path.getsize(filename) / 8
 
     def read(self, nframes=10, lframes=1024, sframes=0):
         self.read_samples(nframes * lframes, offset=sframes * lframes)
+
+    def read_complete_file(self):
+        """
+        Read a complete TDMS file
+        :return:
+        """
+        filesize = os.path.getsize(self.filename)
+        self.nsamples_total = filesize / 8
+        self.data_array = np.fromfile(self.filename, dtype=np.complex64)
+
 
     def read_samples(self, nsamples, offset=0):
 
